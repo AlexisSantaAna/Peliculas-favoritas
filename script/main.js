7f88f26708ca9ae0fbe807c58a974423fe515ac4
Alexis Santa Ana <ibanez2112@hotmail.com>
2022-04-30 21:33:10 -0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,86 @@
+//Clase constructora de pelis
+class Pelicula {
+    constructor(titulo, duracion, imdb, linkTrailer) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+        this.imdb = imdb;
+        this.linkTrailer = linkTrailer
+    }
+}
+
+//En caso de haber pelis en localStorage, me las trae a variable. Sino, array vacío.
+let peliculas = JSON.parse(localStorage.getItem('peliculas')) ?? []
+//Al clickear submit, ejecuta escucha de eventos y función agregarPelicula
+document.getElementById('formulario-pelicula').addEventListener('submit', agregarPelicula)
+
+
+function agregarPelicula(e) {
+    e.preventDefault()
+    //Obtengo datos del formulario y los guardo en variables
+    const formulario = new FormData(e.target)
+    const titulo = formulario.get('titulo')
+    const duracion = formulario.get('duracion')
+    const imdb = formulario.get('imdb')
+    const linkTrailer = formulario.get('linkTrailer')
+    const pelicula = new Pelicula(titulo, duracion, imdb, linkTrailer)
+    //Si la validación da true, se agrega pelicula. Si da false no retorna nada.
+    if (camposCorrectos(pelicula)) {
+        peliculas.push(pelicula)
+        //Crea en localStorage clave: 'peliculas, valor: 'objecto/s del array peliculas' 
+        localStorage.setItem('peliculas', JSON.stringify(peliculas))
+        //
+        mostrarPeliculas()
+        e.target.reset()
+    }
+}
+
+function mostrarPeliculas() {
+    let listadoDePeliculas = document.getElementById('listadoDePeliculas')
+    listadoDePeliculas.innerHTML = ""
+
+    peliculas.forEach((pelicula) => {
+        let div = document.createElement('div')
+        div.className = "col-4";
+        div.innerHTML = `        
+            <div class="card">
+                <div class="card-body id="card-btn">        
+                    <h5 class="card-title">${pelicula.titulo}</h5>
+                    <p class="card-text">Duración: ${pelicula.duracion} minutos.</p>
+                    <a href="${pelicula.imdb}" target="blank">Link IMDB</a><br>
+                    <a href="${pelicula.linkTrailer}" target="blank">Link trailer</a>
+                </div>
+            </div>
+        `
+        const btnBorrar = document.createElement('button')
+        btnBorrar.innerText = "Eliminar"
+        btnBorrar.classList.add('btn', 'btn-primary')
+        btnBorrar.addEventListener('click', () => {
+            eliminarPelicula(pelicula)
+        })
+        div.appendChild(btnBorrar)
+        listadoDePeliculas.appendChild(div)
+    })
+}
+
+function eliminarPelicula(pelicula) {
+    Swal.fire({
+        title: `¿Borrar ${pelicula.titulo}?`,
+        text: "Esta acción es irreversible",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            peliculas = peliculas.filter((item) => item.titulo != pelicula.titulo);
+            localStorage.setItem("peliculas", JSON.stringify(peliculas));
+            mostrarPeliculas();
+            Swal.fire("Eliminada!", "La película ha sido eliminada", "success");
+        }
+    });
+}
+
+mostrarPeliculas();
+
+
---
+++ script/validaciones.js
@@ -0,0 +1,14 @@
+function camposCorrectos({ titulo, duracion, imdb, linkTrailer }) {
+    if (titulo == "" || titulo.trim() == "" || duracion == "" || duracion.trim() == "" || imdb == "" || imdb.trim() == "" || linkTrailer == "" || linkTrailer.trim() == "") {
+      Swal.fire({
+        title: "Error!",
+        text: "No puede haber campos vacíos",
+        imageUrl: "../assets/error.png",
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: "Custom image"
+      });
+      return false;
+    }
+    return true;
+  }
